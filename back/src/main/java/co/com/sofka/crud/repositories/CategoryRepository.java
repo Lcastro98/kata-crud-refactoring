fc1983eccde9744bc9c9992b90dbcdff73c6f5e9
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-05 23:36:17 -0500

    Repositorio
    Se agrega el repositorio de categoria, el cual facilita el acceso a los datos

--- back/src/main/java/co/com/sofka/crud/repositories/CategoryRepository.java
+++ back/src/main/java/co/com/sofka/crud/repositories/CategoryRepository.java
@@ -1,2 +1,7 @@
-package co.com.sofka.crud.repositories;public interface CategoryRepository {
+package co.com.sofka.crud.repositories;
+
+import co.com.sofka.crud.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
