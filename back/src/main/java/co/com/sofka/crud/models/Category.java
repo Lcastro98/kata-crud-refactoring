f839e821dfce7501d6fd0df6245ed442f5d1faeb
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-05 23:34:07 -0500

    Modelos
    Se agrega la entidad categoria y se modifica la entidad todo, de igual forma se hace la relaccion correspondiente entre ellas

--- back/src/main/java/co/com/sofka/crud/models/Category.java
+++ back/src/main/java/co/com/sofka/crud/models/Category.java
@@ -1,2 +1,45 @@
-package co.com.sofka.crud.models;public class Categoty {
+package co.com.sofka.crud.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false, name="cat_id")
+    private Long id;
+
+    @Column(name="cat_title")
+    private String title;
+
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            targetEntity = Todo.class,
+            cascade = CascadeType.REMOVE,
+            mappedBy = "category")
+    @JsonManagedReference
+    private List<Todo> todo;
 }
--- back/src/main/java/co/com/sofka/crud/models/Todo.java
+++ back/src/main/java/co/com/sofka/crud/models/Todo.java
@@ -1,47 +1,47 @@
 package co.com.sofka.crud.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
+@Table(name="todo")
 public class Todo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false, name="todo_id")
     private Long id;
-    private String name;
+
+    @Column(name="todo_text")
+    private String text;
+
+    @Column(name="todo_completed")
     private boolean completed;
-    private String groupListId;
 
-    public String getGroupListId() {
-        return groupListId;
-    }
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            targetEntity = Category.class,
+            optional = false)
+    @JoinColumn(name="category")
+    @JsonBackReference
+    private Category category;
 
-    public void setGroupListId(String groupListId) {
-        this.groupListId = groupListId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
