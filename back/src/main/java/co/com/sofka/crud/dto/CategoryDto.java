fd8d7771669c6a08b07c31e6e964b389a255e1fe
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-06 00:12:08 -0500

    DTO
    Se agregan los dto de categoria y todo

--- back/src/main/java/co/com/sofka/crud/dto/CategoryDto.java
+++ back/src/main/java/co/com/sofka/crud/dto/CategoryDto.java
@@ -1,2 +1,20 @@
-package co.com.sofka.crud;public class CategoryDto {
+package co.com.sofka.crud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String title;
+    private List<TodoDto> todo;
 }
--- back/src/main/java/co/com/sofka/crud/dto/TodoDto.java
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDto.java
@@ -1,2 +1,20 @@
-package co.com.sofka.crud;public class TodoDto {
+package co.com.sofka.crud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String text;
+    private boolean completed;
+    private CategoryDto categoryDto;
 }
