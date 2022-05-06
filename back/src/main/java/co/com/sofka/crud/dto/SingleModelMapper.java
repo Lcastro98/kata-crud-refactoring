267b9553659b8bfc92564240772f59fd4ee70cf8
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-06 00:15:07 -0500

    Mapper
    Se agrega la dependencia model mapper y se crea la clase correspondiente para realizar el mapeo de los datos, de la entidad al dto y viceversa

--- back/src/main/java/co/com/sofka/crud/dto/SingleModelMapper.java
+++ back/src/main/java/co/com/sofka/crud/dto/SingleModelMapper.java
@@ -1,2 +1,26 @@
-package co.com.sofka.crud;public class ModelMapper {
+package co.com.sofka.crud.dto;
+
+import co.com.sofka.crud.models.Category;
+import co.com.sofka.crud.models.Todo;
+import org.modelmapper.ModelMapper;
+
+public class SingleModelMapper {
+    private final ModelMapper mapper = new ModelMapper();
+
+    public Category mapToCategory(CategoryDto categoryDto) {
+        return mapper.map(categoryDto, Category.class);
+    }
+
+    public Todo mapToTodo(TodoDto todoDto) {
+        return mapper.map(todoDto, Todo.class);
+    }
+
+    public CategoryDto mapToCategoryDto(Category category) {
+        return mapper.map(category, CategoryDto.class);
+    }
+
+    public TodoDto mapToTodoDto(Todo todo) {
+        return mapper.map(todo, TodoDto.class);
+    }
+
 }
