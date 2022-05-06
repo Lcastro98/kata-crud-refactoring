0f51ec859bf906ca2859af2e6a4141a3e52767ab
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-06 00:03:22 -0500

    Servicio
    Se agrega el servicio de categoria

--- back/src/main/java/co/com/sofka/crud/services/CategoryService.java
+++ back/src/main/java/co/com/sofka/crud/services/CategoryService.java
@@ -1,2 +1,29 @@
-package co.com.sofka.crud.services;public class CategoryService {
+package co.com.sofka.crud.services;
+
+import co.com.sofka.crud.models.Category;
+import co.com.sofka.crud.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public Iterable<Category> list() {
+        return categoryRepository.findAll();
+    }
+
+    public Category save(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id){
+        categoryRepository.delete(get(id));
+    }
+
+    public Category get(Long id){
+        return categoryRepository.findById(id).orElseThrow();
+    }
 }
