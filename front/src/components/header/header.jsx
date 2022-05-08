054327fa039be2d473b09276933e1f5bb1a240cc
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-08 10:33:51 -0500

    Header
    se adiciona el header del programa con el titulo del mismo

---
+++ front/src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => (
+    <div className='container'>
+        <h1 className="text-center">
+            <img alt="" src="https://icons.veryicon.com/png/o/business/data-chart-display/dashboard-85.png" width="50" height="50"/>
+            Dashboard
+            <img alt="" src="https://icons.veryicon.com/png/o/business/data-chart-display/dashboard-85.png" width="50" height="50"/>
+            </h1>
+        <hr/>
+    </div>
+)
+ 
+export default Header;
